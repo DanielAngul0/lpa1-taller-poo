9d48f21828fe920733f6a441d43968349dc54f0b
DanielAngul0 <daniel.andres.angulo.perez>
2025-03-25 20:52:15 -0500

    Definiendo clase abstracta 'Mueble' con atributos comunes y metodos abstractos.

--- muebleria/mueble.py
+++ muebleria/mueble.py
@@ -1,18 +1,19 @@
 # Importaciones necesarias
 from abc import ABC, abstractmethod
 
-# clases relacionadas con los muebles
+# --- Clase abstracta (Base) ---
 class Mueble(ABC):
     def __init__(self, material, precio):
         self.material = material
         self.precio = precio
 
-    # metodo abstracto que sera implementado por las subclases
+    # Metodo abstracto que sera implementado por las subclases
     @abstractmethod
     def calcular_precio_final(self):
         pass
 
+    # Muestra el nombre de la clase del objeto seguido de sus atributos principales en un formato string legible
     def __str__(self):
         return f"""---{self.__class__.__name__}---
-Mueble con material: {self.material}
-con un precio de: {self.precio}"""
+Material del mueble: {self.material}
+Precio: $ {self.precio}"""
