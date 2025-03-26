d638c118f4a1bb66f8c36b8d7fc9fd7dedae6336
DanielAngul0 <daniel.andres.angulo.perez>
2025-03-26 00:55:53 -0500

    Creando objetos de cada subclase para mostrar los datos obtenidos.

--- muebleria/main.py
+++ muebleria/main.py
@@ -1,6 +1,15 @@
-# ejecutar el programa y probar las clases
-from mesa import Mesa
+# Importaciones de las subclases para los objetos
+from silla import Silla # Importa material, precio y numero de patas de la clase Silla
+from mesa import Mesa # Importa material, precio y dimensiones de la clase Mesa
+from armario import Armario # Importa material, precio y numero de puertas de la clase Armario
 
-m = Mesa("madera", 5000, 100, 150)
+# Creando objetos de cada clase
+# Datos representados: Material, precio y caracteristica propia de cada mueble
+silla = Silla("Cuero sintetico", 411621, 4) # 4 patas
+mesa = Mesa("Madera", 508057, 46, 101) # 46x101 cm
+armario = Armario("MDP", 749900, 3) # 3 puertas
 
-print(m)
+# Imprimiendo datos de los objetos en cadena de texto
+print(silla)
+# print(mesa)
+# print(armario)
